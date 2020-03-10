70295fd4568a87dd3309f8a77ec07ab3a49417cc
Pedro <soarerobert17@gmail.com>
2020-03-10 18:06:34 +0200

    generare steag 3 culori

--- Laborator/main.cpp
+++ Laborator/main.cpp
@@ -43,9 +43,38 @@ void generare_steag(int dim) {
     }
 }
 
+void generare_steag_3culori(int culoare_mijlor) {
+    string culori[8] = {"","rosu", "oranj", "galben", "verde", "albastru", "indigo", "violet"};
+    int sol[8], k = 1, dim = 2;
+    bool ok = false;
+
+    // Generare
+    sol[k] = 0;
+    while(k > 0) {
+        ok = false;
+
+        while( !ok && sol[k] + 1 <= 7 ) {
+            sol[k] += 1;
+            if(validare_steag(sol, k) && sol[k] != culoare_mijlor) {
+                ok = true;
+            }
+        }
+
+        if(!ok) {
+            k --;
+        } else if(k == dim) {
+            cout << culori[sol[1]] <<" "<< culori[culoare_mijlor] <<" "<< culori[sol[2]];
+            cout << endl;
+        } else {
+            k ++;
+            sol[k] = 0;
+        }
+    }
+}
 
 int main()
 {
-    generare_steag(3);
+    // generare_steag(3);
+    generare_steag_3culori(3);
     return 0;
 }

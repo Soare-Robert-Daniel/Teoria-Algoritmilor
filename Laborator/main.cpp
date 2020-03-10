a1c8703f055bfaa89fea5bf6ba190f1d09f8a49f
Pedro <soarerobert17@gmail.com>
2020-03-10 18:40:08 +0200

    generare muchii cub

--- Laborator/main.cpp
+++ Laborator/main.cpp
@@ -72,9 +72,88 @@ void generare_steag_3culori(int culoare_mijlor) {
     }
 }
 
+bool validare_permutare(int sol[13], int k) {
+    for(int i = 1; i < k; ++i) {
+        if(sol[i] == sol[k]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool validare_sume_muchii(int sol[13], int fete[6][4]) {
+    int suma_necesara = 26;
+
+    for(int fata = 0; fata < 6; fata++) {
+        int suma_fata = 0;
+        for(int i = 0; i < 4; ++i) {
+            suma_fata += sol[fete[fata][i]];
+        }
+
+        if(suma_fata != suma_necesara) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void generare_muchii_cub() {
+    int sol[13], k = 1, nr_muchii = 12, dim = 12;
+    int fete[6][4] = {
+        {1, 2, 3, 4},
+        {4, 5, 6, 7},
+        {7, 8, 9, 1},
+        {6, 8, 10, 12},
+        {2, 9, 10, 11},
+        {3, 5, 12, 11}
+    };
+
+    bool ok = false;
+
+    sol[k] = 0;
+
+    while(k > 0) {
+        ok = false;
+
+        while( !ok && sol[k] + 1 <= nr_muchii) {
+            sol[k] += 1;
+
+            if(validare_permutare(sol, k)) {
+                ok = true;
+            }
+        }
+
+        if(!ok) {
+            k --;
+        } else if(k == nr_muchii && validare_sume_muchii(sol, fete)) {
+            for(int i = 1; i <= dim; ++i) {
+                cout << sol[i] <<" ";
+            }
+
+            cout << endl;
+            for (int fata = 0; fata < 6; fata++)
+            {
+                cout << "Fata " << fata << ": ";
+                for (int i = 0; i < 4; ++i)
+                {
+                    cout << sol[fete[fata][i]] << " ";
+                }
+                cout << endl;
+            }
+
+            cout << endl;
+        } else {
+            k ++;
+            sol[k] = 0;
+        }
+    }
+}
+
 int main()
 {
     // generare_steag(3);
-    generare_steag_3culori(3);
+    //generare_steag_3culori(3);
+    generare_muchii_cub();
     return 0;
 }
